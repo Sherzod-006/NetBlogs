5a5880b50ace7319f8f075595090faec5b9da5de
Sherzod Sobirov <sherzodsobirovv1@gmail.com>
2025-11-23 01:14:38 +0500

    fixing

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const cors = require("cors");
 dotenv.config();
 const app = express();
 
-app.use(cors({ origin: "*" }));
+app.use(cors({ origin: "*", methods: "GET,POST,PUT,DELETE,PATCH" }));
 app.use(express.json());
 
 //IMPORTING ROUTES
