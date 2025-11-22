021491c8349a3b6c9559aa073fa0e3a4bc03c7da
Sherzod Sobirov <sherzodsobirovv1@gmail.com>
2025-11-23 01:26:47 +0500

    fixing

--- backend/routes/ImageUploadRoute.js
+++ backend/routes/ImageUploadRoute.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const { storage } = require("../db/Cloudinary");
 const multer = require("multer");
+const User = require("../models/UserModel");
 
 const router = express.Router();
 const upload = multer({ storage });
 
 router.post("/upload", upload.single("image"), async (req, res) => {
-  const userId = "req.body.userId";
+  const userId = req.body.userId;
   const imageUrl = req.file.path; // Cloudinary URL
   res.json({ message: "Image Uploaded", imageUrl, userId });
+  User.findByIdAndUpdate(userId, { profileImage: imageUrl });
 });
 
 module.exports = router;
