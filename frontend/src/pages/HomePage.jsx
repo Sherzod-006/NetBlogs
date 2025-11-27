a217966c1b625587d4468b095881448a37766855
Sherzod Sobirov <sherzodsobirovv1@gmail.com>
2025-11-27 23:49:49 +0200

    building...

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,13 +1,13 @@
 const HomePage = () => {
   return (
-    <ul className="bg-white dark:bg-gray-700 text-black dark:text-white h-170  md:h-130 md:w-2/3 opacity-70 m-1 md:m-3 rounded-lg shadow-lg p-2 md:p-3 overflow-auto border-2 border-gray-300 dark:border-gray-600">
+    <ul className="bg-white dark:bg-gray-700 text-black dark:text-white h-170  md:h-130 md:w-2/3 opacity-70 m-1 md:m-3 rounded-lg shadow-lg p-2 md:p-3 overflow-auto">
       <li>
         <section className="flex space-x-2 md:space-x-4 ">
           <img
             src="https://res.cloudinary.com/dzqw71hhs/image/upload/v1764249504/myapp/kidj0nlfkhkj5ugmx1ne.jpg"
             className="rounded-full w-9 h-9 object-cover"
           />
-          <div className="space-y-1">
+          <div className="space-y-2 md:space-y-4">
             <h1 className="font-medium text-2xl">Welcome to NetBlogs</h1>
             <p className="text-lg">
               Your go-to platform for insightful blogs and articles.
@@ -18,7 +18,7 @@ const HomePage = () => {
             />
           </div>
         </section>
-        <section className="flex justify-around mx-2 md:mx-4 w-4/5">
+        <section className="flex justify-around mx-2 md:mx-4 w-4/5  border-2 border-gray-300 dark:border-gray-600">
           <button>Like</button>
           <button>Comment</button>
           <button>Share</button>
