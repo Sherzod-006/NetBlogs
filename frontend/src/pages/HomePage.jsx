ff4aa067cfb685c04156438e3ffe84db3200b9d7
Sherzod Sobirov <sherzodsobirovv1@gmail.com>
2025-11-27 23:28:53 +0200

    building...

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,7 +2,7 @@ const HomePage = () => {
   return (
     <div>
       <main className="bg-white dark:bg-gray-700 text-black dark:text-white h-170  md:h-130 md:w-2/3 opacity-70 m-1 md:m-3 rounded-lg shadow-lg p-2 md:p-3 overflow-auto">
-        <section className="flex items-end">
+        <section className="flex space-x-2 md:space-x-4 ">
           <img
             src="https://res.cloudinary.com/dzqw71hhs/image/upload/v1764249504/myapp/kidj0nlfkhkj5ugmx1ne.jpg"
             className="rounded-full w-9 h-9 object-cover"
