a432edce9423787a727fad1ebaed2a179db08931
Sherzod Sobirov <sherzodsobirovv1@gmail.com>
2025-11-27 16:29:41 +0200

    b

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,7 @@
 const HomePage = () => {
   return (
     <div>
-      <main className="bg-white dark:bg-gray-700 text-black dark:text-white h-170  md:h-130 opacity-70 m-1 md:m-3 rounded-lg shadow-lg p-2 md:p-3">
+      <main className="bg-white dark:bg-gray-700 text-black dark:text-white h-170  md:h-130 md:w-150 opacity-70 m-1 md:m-3 rounded-lg shadow-lg p-2 md:p-3">
         hello
       </main>
     </div>
