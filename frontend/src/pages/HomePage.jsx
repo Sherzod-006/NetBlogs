36f96d411b02bf4cc681e907962a3173383ecab3
Sherzod Sobirov <sherzodsobirovv1@gmail.com>
2025-11-28 00:06:07 +0200

    b

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,11 +1,11 @@
 const HomePage = () => {
   return (
     <ul className="bg-white dark:bg-gray-700 text-black dark:text-white h-170  md:h-130 md:w-2/3 opacity-70 m-1 md:m-3 rounded-lg shadow-lg p-2 md:p-3 overflow-auto">
-      <li className="border-b-4 border-gray-500 dark:border-gray-300">
+      <li className="border-b-4 border-gray-500 dark:border-gray-300 mb-4">
         <section className="flex space-x-2 md:space-x-4 ">
           <img
             src="https://res.cloudinary.com/dzqw71hhs/image/upload/v1764249504/myapp/kidj0nlfkhkj5ugmx1ne.jpg"
-            className="rounded-full w-9 h-9 object-cover"
+            className="rounded-full w-9 h-9 object-cover p-2"
           />
           <div className="space-y-1 md:space-y-2">
             <h1 className="font-medium text-2xl">Welcome to NetBlogs</h1>
