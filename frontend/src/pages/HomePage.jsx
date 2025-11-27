d29ee021543bfc32f493b0ff3e03137f9d19b250
Sherzod Sobirov <sherzodsobirovv1@gmail.com>
2025-11-27 23:07:16 +0200

    b

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,7 @@
 const HomePage = () => {
   return (
     <div>
-      <main className="bg-white dark:bg-gray-700 text-black dark:text-white h-170  md:h-130 md:w-2/3 opacity-70 m-1 md:m-3 rounded-lg shadow-lg p-2 md:p-3 overflow-y-scroll scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-gray-100">
+      <main className="bg-white dark:bg-gray-700 text-black dark:text-white h-170  md:h-130 md:w-2/3 opacity-70 m-1 md:m-3 rounded-lg shadow-lg p-2 md:p-3 overflow-auto ">
         sher vitae saepe adipisci at quo necessitatibus deserunt quia nostrum
         aliquam repellat rem nam. Culpa ipsum voluptates quia quas neque labore
         dolor optio quae dolorem! Labore ipsa porro aspernatur, magnam dolor
