fa179933710cec5df8e141341b2d22ac126b3746
Sherzod Sobirov <sherzodsobirovv1@gmail.com>
2025-11-27 16:05:13 +0200

    building

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,9 @@
 const HomePage = () => {
   return (
     <div>
-      <main className="bg-amber-300 md:h-130 opacity-50">hello</main>
+      <main className="bg-white dark:bg-gray-700 text-black dark:text-white md:h-130 opacity-50 m-1 md:m-3 rounded-lg shadow-lg">
+        hello
+      </main>
     </div>
   );
 };
