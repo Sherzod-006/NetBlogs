580fdf7da152a0ec069142f20efa2e3bd678a262
Sherzod Sobirov <sherzodsobirovv1@gmail.com>
2025-11-28 00:15:26 +0200

    updating...

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,11 +1,11 @@
 const HomePage = () => {
   return (
     <ul className="bg-white dark:bg-gray-700 text-black dark:text-white h-170  md:h-130 md:w-2/3 opacity-70 m-1 md:m-3 rounded-lg shadow-lg p-2 md:p-3 overflow-auto">
-      <li className="border-b-4 border-gray-500 dark:border-gray-300 mb-2">
+      <li className="border-b-4 border-gray-500 dark:border-gray-300 mb-2 cursor-pointer">
         <section className="flex space-x-2 md:space-x-4 ">
           <img
             src="https://res.cloudinary.com/dzqw71hhs/image/upload/v1764249504/myapp/kidj0nlfkhkj5ugmx1ne.jpg"
-            className="rounded-full w-9 h-9 object-cover mt-4"
+            className="rounded-full w-9 h-9 object-cover mt-2"
           />
           <div className="space-y-1 md:space-y-2">
             <h1 className="font-medium text-2xl">Welcome to NetBlogs</h1>
@@ -14,7 +14,7 @@ const HomePage = () => {
             </p>
             <img
               src="https://res.cloudinary.com/dzqw71hhs/image/upload/v1763387692/main-sample.png"
-              alt=""
+              className="w-auto h-auto rounded-lg object-cover"
             />
           </div>
         </section>
