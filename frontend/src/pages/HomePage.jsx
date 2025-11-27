b8522e682cbcf7602eed641b6da7b53bef7f792c
Sherzod Sobirov <sherzodsobirovv1@gmail.com>
2025-11-27 23:25:32 +0200

    creating...

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,10 +2,10 @@ const HomePage = () => {
   return (
     <div>
       <main className="bg-white dark:bg-gray-700 text-black dark:text-white h-170  md:h-130 md:w-2/3 opacity-70 m-1 md:m-3 rounded-lg shadow-lg p-2 md:p-3 overflow-auto">
-        <section>
+        <section className="flex align-bottom">
           <img
             src="https://res.cloudinary.com/dzqw71hhs/image/upload/v1764249504/myapp/kidj0nlfkhkj5ugmx1ne.jpg"
-            alt=""
+            className="rounded-full w-9 h-9 object-cover"
           />
           <div>
             <h1>Welcome to NetBlogs</h1>
