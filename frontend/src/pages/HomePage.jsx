4463488dc15028e41e78a0e2b0306771e0d8dc96
Sherzod Sobirov <sherzodsobirovv1@gmail.com>
2025-12-01 22:24:43 +0200

    c

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -88,64 +88,6 @@ const HomePage = () => {
                 Follow
               </button>
             </Link>
-
-            <Link
-              to="/profile/user123"
-              className="h-10 flex bg-gray-200 dark:bg-gray-600 rounded-lg justify-between items-center w-13/14 p-1"
-            >
-              <img
-                src="https://res.cloudinary.com/dzqw71hhs/image/upload/v1764249504/myapp/kidj0nlfkhkj5ugmx1ne.jpg"
-                className="h-14 w-14 rounded-full relative right-7 z-50"
-              />
-              <h1 className="font-bold text-lg ml-2">Lion</h1>
-              <a
-                href="http://gmail.com"
-                className="text-gray-400 hover:underline hover:text-orange-500"
-              >
-                sherzodsobirovv1@gmail.com
-              </a>
-              <button className="bg-orange-500 text-white px-3 py-1 rounded hover:bg-orange-600 cursor-pointer shadow-lg font-medium">
-                Follow
-              </button>
-            </Link>
-          </li>
-          <li className="h-10 flex bg-gray-200 dark:bg-gray-600 rounded-lg justify-between items-center w-13/14 p-1">
-            <Link to="/profile/user123" className="">
-              <img
-                src="https://res.cloudinary.com/dzqw71hhs/image/upload/v1764249504/myapp/kidj0nlfkhkj5ugmx1ne.jpg"
-                className="h-14 w-14 rounded-full relative right-7 z-50"
-              />
-              <h1 className="font-bold text-lg ml-2">Lion</h1>
-              <a
-                href="http://gmail.com"
-                className="text-gray-400 hover:underline hover:text-orange-500"
-              >
-                sherzodsobirovv1@gmail.com
-              </a>
-              <button className="bg-orange-500 text-white px-3 py-1 rounded hover:bg-orange-600 cursor-pointer shadow-lg font-medium">
-                Follow
-              </button>
-            </Link>
-
-            <Link
-              to="/profile/user123"
-              className="h-10 flex bg-gray-200 dark:bg-gray-600 rounded-lg justify-between items-center w-13/14 p-1"
-            >
-              <img
-                src="https://res.cloudinary.com/dzqw71hhs/image/upload/v1764249504/myapp/kidj0nlfkhkj5ugmx1ne.jpg"
-                className="h-14 w-14 rounded-full relative right-7 z-50"
-              />
-              <h1 className="font-bold text-lg ml-2">Lion</h1>
-              <a
-                href="http://gmail.com"
-                className="text-gray-400 hover:underline hover:text-orange-500"
-              >
-                sherzodsobirovv1@gmail.com
-              </a>
-              <button className="bg-orange-500 text-white px-3 py-1 rounded hover:bg-orange-600 cursor-pointer shadow-lg font-medium">
-                Follow
-              </button>
-            </Link>
           </li>
         </ul>
       </main>
