5152d0329d5f4d5ac4d40ae4d879453c61704018
Sherzod Sobirov <sherzodsobirovv1@gmail.com>
2025-12-01 21:28:13 +0200

    l

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -59,7 +59,7 @@ const HomePage = () => {
         </li>
       </ul>
       <ul className="bg-white dark:bg-gray-700 text-black dark:text-white h-170  md:h-130 md:w-2/4 m-1 md:m-3 rounded-lg shadow-lg p-2 md:p-3 overflow-auto space-y-3">
-        <div>
+        <div className="flex space-x-2">
           <input
             type="text"
             className="bg-gray-400 px-4 py-1 outline-none border-none rounded-2xl text-black"
