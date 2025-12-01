41f63cc182d9b5a51a47f4471367cbdcdfe49c73
Sherzod Sobirov <sherzodsobirovv1@gmail.com>
2025-12-01 21:13:56 +0200

    ss

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -58,24 +58,22 @@ const HomePage = () => {
         </li>
       </ul>
       <ul className="bg-white dark:bg-gray-700 text-black dark:text-white h-170  md:h-130 md:w-2/4 m-1 md:m-3 rounded-lg shadow-lg p-2 md:p-3 overflow-auto">
-        <li className="cursor-pointer flex space-y-3">
+        <li className="flex bg-gray-200 dark:bg-gray-600 rounded-lg justify-center items-center">
           <h1>hello</h1>
+        </li>
+        <li className="flex bg-gray-200 dark:bg-gray-600 rounded-lg justify-center items-center">
           <h1>hello</h1>
+        </li>
+        <li className="flex bg-gray-200 dark:bg-gray-600 rounded-lg justify-center items-center">
           <h1>hello</h1>
+        </li>
+        <li className="flex bg-gray-200 dark:bg-gray-600 rounded-lg justify-center items-center">
           <h1>hello</h1>
+        </li>
+        <li className="flex bg-gray-200 dark:bg-gray-600 rounded-lg justify-center items-center">
           <h1>hello</h1>
-          <h1>hello</h1>
-          <h1>hello</h1>
-          <h1>hello</h1>
-          <h1>hello</h1>
-          <h1>hello</h1>
-          <h1>hello</h1>
-          <h1>hello</h1>
-          <h1>hello</h1>
-          <h1>hello</h1>
-          <h1>hello</h1>
-          <h1>hello</h1>
-          <h1>hello</h1>
+        </li>
+        <li className="flex bg-gray-200 dark:bg-gray-600 rounded-lg justify-center items-center">
           <h1>hello</h1>
         </li>
       </ul>
