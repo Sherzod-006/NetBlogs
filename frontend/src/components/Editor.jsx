5da563952fcf08dd55d392426336e19e7603a0b9
Sherzod Sobirov <sherzodsobirovv1@gmail.com>
2025-12-01 11:45:48 +0200

    b

--- frontend/src/components/Editor.jsx
+++ frontend/src/components/Editor.jsx
@@ -19,7 +19,6 @@ export default function Editor({ value, onChange }) {
       onChange={onChange}
       plugins={[
         listsPlugin(),
-        CreateLink(),
         toolbarPlugin({
           toolbarContents: () => (
             <>
