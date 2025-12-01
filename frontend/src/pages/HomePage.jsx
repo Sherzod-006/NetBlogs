36d480f1c72476eb17e3e4504460b7ceb2c14c90
Sherzod Sobirov <sherzodsobirovv1@gmail.com>
2025-12-01 11:00:56 +0200

    updating...

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -57,7 +57,7 @@ const HomePage = () => {
           </form>
         </li>
       </ul>
-      <main className="bg-white dark:bg-gray-700 text-black dark:text-white rounded-lg shadow-lg p-4"></main>
+      <main className="bg-white dark:bg-gray-700 text-black dark:text-white h-170  md:h-130 md:w-2/4 m-1 md:m-3 rounded-lg shadow-lg p-2 md:p-3 overflow-auto"></main>
     </main>
   );
 };
